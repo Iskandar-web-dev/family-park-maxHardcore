9111d633afd6e218b7eb8ec82543271e19e79c49
iskandar <106257364+Iskandar-web-dev@users.noreply.github.com>
2023-02-15 12:45:28 +0500

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,109 @@
+let one = document.querySelector('.one')
+let two = document.querySelector('.two')
+let three = document.querySelector('.three')
+let four = document.querySelector('.four')
+let five = document.querySelector('.five')
+let six = document.querySelector('.six')
+let seven = document.querySelector('.seven')
+let eight = document.querySelector('.eight')
+let nine = document.querySelector('.nine')
+let ten = document.querySelector('.ten')
+let eleven = document.querySelector('.eleven')
+let twelf = document.querySelector('.twelf')
+let thirteen = document.querySelector('.thirteen')
+let fourteen = document.querySelector('.fourteen')
+let fifteen = document.querySelector('.fifteen')
+let sixtenn = document.querySelector('.sixtenn')
+let seventeen = document.querySelector('.seventeen')
+let eighteen = document.querySelector('.eighteen')  
+            
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ one.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ two.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ three.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ four.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ five.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ six.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ seven.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ eight.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ nine.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ ten.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ eleven.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ twelf.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ thirteen.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ fourteen.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ fifteen.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ sixtenn.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+ seventeen.append(el)
+}
+for (let i = 0; i <= 17; i++) {
+ let el = document.createElement('div')
+ el.classList.add('el')
+eighteen.append(el)
+}
